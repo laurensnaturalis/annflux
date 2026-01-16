7fcaf007d02e947c0a89469ca4566c491c105326
Laurens Hogeweg (Naturalis) <laurens.hogeweg@naturalis.nl>
2026-01-16 11:18:41 +0100

    stuff

--- src/annflux/projects/al_evaluation/plot_vals_papbig.py
+++ src/annflux/projects/al_evaluation/plot_vals_papbig.py
@@ -9,11 +9,12 @@ from pandas import DataFrame
 
 def m():
     accuracy_avg, recall_avg, t = compute_averages("experiments/papbig_knnexp_5_step_500_strategy_fre_strat_linear_at_(0, 5, 10, 15)")
-    # accuracy_avg2, recall_avg2, t = compute_averages("experiments/step_500_strategy_fre_strat_linear_at_1")
+    accuracy_avg2, recall_avg2, t = compute_averages("experiments/papbig_knnexp_10_step_500_strategy_fre_strat_linear_at_(0, 5, 10, 15)")
     # accuracy_avg3, recall_avg3, t = compute_averages("experiments/step_500_strategy_fre_strat_linear_at_(1, 5, 10)")
     # accuracy_avg4, recall_avg4, t = compute_averages("experiments/papbig_step_500_strategy_fre_strat_linear_at_(0, 5, 10, 15)")
     plt.subplot(221)
-    plt.plot(t.labeled_train_set_size, accuracy_avg, label="linear at (0, 5, 10, 15)")
+    plt.plot(t.labeled_train_set_size, accuracy_avg, label="linear at (0, 5, 10, 15), exp = 5")
+    plt.plot(t.labeled_train_set_size, accuracy_avg2, label="linear at (0, 5, 10, 15), exp = 10")
     # plt.plot(t.labeled_train_set_size, accuracy_avg2, label="linear at 1")
     # plt.plot(t.labeled_train_set_size, accuracy_avg3, label="linear at (1, 5, 10)")
     # plt.plot(t.labeled_train_set_size, accuracy_avg4, label="linear at (1, 5, 10)")
@@ -25,7 +26,7 @@ def m():
 
     plt.subplot(222)
     plt.plot(t.labeled_train_set_size, recall_avg, label="Recall")
-    # plt.plot(t.labeled_train_set_size, recall_avg2, label="Recall")
+    plt.plot(t.labeled_train_set_size, recall_avg2, label="Recall")
     # plt.plot(t.labeled_train_set_size, recall_avg3  , label="Recall")
     # plt.plot(t.labeled_train_set_size, recall_avg4  , label="Recall")
     # plt.plot(t2.labeled_train_set_size, t2.recall, label="Recall")
